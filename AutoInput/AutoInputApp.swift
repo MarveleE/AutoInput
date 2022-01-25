4d923bd7e019ff1136330a716ee9950efbf775c1
陈凝 <173619601@qq.com>
2022-01-25 13:56:34 +0800

    Initial Commit

---
+++ AutoInput/AutoInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoInputApp.swift
+//  AutoInput
+//
+//  Created by 陈凝 on 2022/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoInput/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutoInput
+//
+//  Created by 陈凝 on 2022/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
